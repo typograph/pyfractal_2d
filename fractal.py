0cacf50eeab9c9b96845be5edb054ce55a481546
Tim Fuchs <typograph@elec.ru>
2016-11-21 17:51:50 +0100

    Ressurected zero handling

--- fractal.py
+++ fractal.py
@@ -12,12 +12,25 @@ The color None expands to the smallest possible None matrix.
 from numpy import array, zeros
 
 class StepMatrix:
+    """A fractal structure that knows its matrix at every step.
+    
+    The StepMatrix is constructed from a 2D integer numpy array,
+    and uses the internal __color__ table at each step to
+    construct the next one. Zero always expands to itself.
+    """
     
     colors = {}
 
     @classmethod
-    def add_color(cls, color, stepmatrix):
-        cls.colors[color] = stepmatrix
+    def add_color(cls, stepmatrix):
+        """Adds a color to the expansion and return its index.
+        """
+        if cls.colors:
+            i = max(cls.colors.keys())
+        else:
+            i = 0
+        cls.colors[i+1] = stepmatrix
+        return i+1
     
     def __init__(self, matrix):
         self.depths = [ array(matrix, dtype="u1") ]        
@@ -25,6 +38,10 @@ class StepMatrix:
         self.clset = {cell for row in self.depths[0] for cell in row}
 
     def at_depth(self, n):
+        """Returns the matrix after `n` substitutions.
+        
+        The result is cached.
+        """
         if len(self.depths) <= n:
             self.depths.extend([None]*(n-len(self.depths)+1))
         if self.depths[n] is None:
@@ -32,8 +49,10 @@ class StepMatrix:
         return self.depths[n]
             
     def expand(self, n):
+        """Calculates the matrix after `n` substitutions.
+        """
         
-        submatrices = {k:self.colors[k].at_depth(n-1) for k in self.clset}
+        submatrices = {k:self.colors[k].at_depth(n-1) for k in self.clset if k!=0}
         
         basis = self.at_depth(0)
         
@@ -44,19 +63,24 @@ class StepMatrix:
         
         for x, row in enumerate(basis):
             for y, cell in enumerate(row):
+                if cell == 0: # Zero is not expanded
+                    continue
+                
+                hh, ww = submatrices[cell].shape
+                
                 if widths[y] is None:
-                    widths[y] = submatrices[cell].shape[1]
-                elif widths[y] != submatrices[cell].shape[1]:
+                    widths[y] = ww
+                elif widths[y] != ww:
                     raise ValueError("Cell {}@({},{}) expands to W={},"
                         " but a previous cell expanded to W={}"
-                        .format(cell, x, y, submatrices[cell].shape[1], widths[y]))
+                        .format(cell, x, y, ww, widths[y]))
                 
                 if heights[x] is None:
-                    heights[x] = submatrices[cell].shape[0]
-                elif heights[x] != submatrices[cell].shape[0]:
+                    heights[x] = hh
+                elif heights[x] != hh:
                     raise ValueError("Cell {}@({},{}) expands to H={}, "
                         "but a previous cell expanded to H={}"
-                        .format(cell, x, y, submatrices[cell].shape[0], heights[x]))
+                        .format(cell, x, y, hh, heights[x]))
                     
         if None in widths:
             raise ValueError("None encountered in column widths : {}".format(widths))
@@ -64,26 +88,22 @@ class StepMatrix:
             raise ValueError("None encountered in row heights : {}".format(heights))
         
         # New empty matrix
-        matrix = zeros((sum(widths), sum(heights)), dtype="u1")
+        matrix = zeros((sum(heights), sum(widths)), dtype="u1")
         
         i, j = 0, 0
         for x, row in enumerate(basis):
+            hh = heights[x]
             for y, cell in enumerate(row):
-                nstep = submatrices[cell]
-                hh, ww = nstep.shape
-                matrix[j:j+hh, i:i+ww] = nstep
+                ww = widths[y]
+                if cell == 0:
+                    i += ww
+                    continue
+                matrix[j:j+hh, i:i+ww] = submatrices[cell]
                 i += ww
             i = 0
             j += hh
         
         return matrix
-    
-    #@classmethod
-    #def expand_all(cls):
-        #expanded_colors = {}
-        #for c in cls.colors:
-            #expanded_colors[c] = cls.colors[c].expand()
-        #cls.colors = expanded_colors
 
 class NoneMatrix(StepMatrix):
     def __init__(self, W, H):
@@ -103,24 +123,28 @@ if __name__ == "__main__":
     else:
         n = 3
 
-    A, B, C = 0, 1, 2
+    A, B, C = 1, 2, 3
 
-    StepMatrix.add_color(A, StepMatrix([[A, A, A],
-                                        [A, C, A],
-                                        [A, A, A]]))
+    # A
+    StepMatrix.add_color(StepMatrix([[A, A, A],
+                                     [A, C, A],
+                                     [A, A, A]]))
     
-    StepMatrix.add_color(B, StepMatrix([[A, B, A],
-                                        [B, C, B],
-                                        [A, B, A]]))
+    # B
+    StepMatrix.add_color(StepMatrix([[A, B, A],
+                                     [B, C, B],
+                                     [A, B, A]]))
     
-    StepMatrix.add_color(C, StepMatrix([[C, C, C],
-                                        [C, B, C],
-                                        [C, C, C]]))
+    # C
+    StepMatrix.add_color(StepMatrix([[C, C, C],
+                                     [C, B, C],
+                                     [C, C, C]]))
 
     fractal = Image.fromarray(StepMatrix.colors[A].at_depth(n), "P")
     palette = ImagePalette.ImagePalette()
     palette.getcolor((0, 0, 0))
-    palette.getcolor((255, 255, 255))
-    palette.getcolor((0, 0, 255))
+    palette.getcolor((10, 10, 10))
+    palette.getcolor((0, 128, 0))
+    palette.getcolor((0, 0, 200))
     fractal.putpalette(palette)
     fractal.save(sys.argv[1])
